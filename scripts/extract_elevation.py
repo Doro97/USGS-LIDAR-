3998320d5982d7aff7b983ab718adefba5069bcf
Doro97 <dorothycheruiyot97@gmail.com>
2021-08-20 22:03:32 +0300

    elavtion script

---
+++ scripts/extract_elevation.py
@@ -0,0 +1,13 @@
+"""A script to calculate the elevation of an area gven the longitude and latitude coordinates"""
+import requests
+import pandas as pd
+
+# script for returning elevation from lat, long, based on open elevation data
+# which in turn is based on SRTM
+def get_elevation(lat, long):
+    query = ('https://api.open-elevation.com/api/v1/lookup'
+             f'?locations={lat},{long}')
+    r = requests.get(query).json()  # json object, various ways you can extract value
+    # one approach is to use pandas json functionality:
+    elevation = pd.io.json.json_normalize(r, 'results')['elevation'].values[0]
+    return elevation
