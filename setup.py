d6ec74d6e1219fb83a16014be12ca23a782bd542
Doro97 <dorothycheruiyot97@gmail.com>
2021-08-21 14:36:41 +0300

    file added

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import _install_setup_requires, setuptools, version
+
+setup(
+    name=''
+    version='0.1.0'
+    author=''
+    author_email=''
+    packages=['']
+    scripts=['']
+    url=''
+    license=''
+    description=''
+    long_description=open(README.txt).read()
+    _install_setup_requires=open(requirements.txt.read())
+)
