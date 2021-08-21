0a1fd884af8f3cf9a22484b26782675f083b1e73
Doro97 <dorothycheruiyot97@gmail.com>
2021-08-21 17:54:58 +0300

    script modified

--- scripts/plots.py
+++ scripts/plots.py
@@ -1,7 +1,7 @@
 import plotly.express as px
 import pandas as pd
 
-def density_mapbox(df,lat,lng,z,radius=10,center=dict(lat,lng),zoom,mapbox_style):
+def density_mapbox(dataframe,lat,lng,z,radius=10,center=dict(lat,lng),zoom,mapbox_style):
     fig = px.density_mapbox()
     fig.show()
 
